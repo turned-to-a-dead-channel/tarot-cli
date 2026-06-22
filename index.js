b3b48bd2567ae184f17d42ff5091771dd07c3d35
J. <turned.to.a.dead.channel@gmail.com>
2026-06-22 09:47:36 -0400

    Add processing messages, loop the menu, add a quit option

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import pentacles from './data/pentacles.json' with { type: "json" };
 import swords from './data/swords.json' with { type: "json" };
 import wands from './data/wands.json' with { type: "json" };
 import { yesOrNoMessages, header } from './utilities/messages.js';
+import readline from "node:readline";
+import { processingMessages } from './utilities/processing-messages.js';
 
 const colorCode = "\x1b[92m";
 const colorCodeBold = "\x1b[1;92m";
@@ -14,31 +16,37 @@ const colorCodeReset = "\x1b[0m";
 const deck = [];
 deck.push(...majorArcana, ...cups, ...pentacles, ...swords, ...wands);
 
-const menu = [
-    {
-        type: 'select', 
-        name: 'option', 
-        message: colorCodeBold + 'Select which spread you\'d like to use to know your destiny:' + colorCodeReset,
-        choices: [ 
-            { 
-                name: colorCode + 'Yes or No\x1b[0m',
-                value: "yesOrNo" 
-            }, 
-            {
-                name: colorCode + 'One Card\x1b[0m',
-                value: "oneCard"
-            }
-        ]
-    }
-]
+async function mainMenu() {
+    const menu = [
+        {
+            type: 'select', 
+            name: 'option', 
+            message: colorCodeBold + 'Select which spread you\'d like to use to know your destiny:' + colorCodeReset,
+            choices: [ 
+                { 
+                    name: colorCode + 'Yes or No' + colorCodeReset,
+                    value: "yesOrNo" 
+                }, 
+                {
+                    name: colorCode + 'One Card' + colorCodeReset,
+                    value: "oneCard"
+                },
+                {
+                    name: colorCode + "I no longer wish to see the future. (Quit)" + colorCodeReset,
+                    value: "quit"
+                }
+            ]
+        }
+    ]
 
-console.clear();
-console.log(colorCodeBold + header + colorCodeReset);
+    console.clear();
+    console.log(colorCodeBold + header + colorCodeReset);
 
-inquirer.prompt(menu).then(answer => {
-    const choice = answer.option;
-    discoverYourDestiny(choice);
-});
+    inquirer.prompt(menu).then(answer => {
+        const choice = answer.option;
+        discoverYourDestiny(choice);
+    });
+}
 
 async function discoverYourDestiny(userChoice) {
     switch (userChoice) {
@@ -48,6 +56,9 @@ async function discoverYourDestiny(userChoice) {
         case "oneCard": 
             oneCardSpread();
             return;
+        case "quit": 
+            console.clear();
+            process.exit();
     }
 }
 
@@ -73,17 +84,50 @@ async function oneCardSpread() {
 
         console.log(colorCodeBold + "You selected " + card.name + "!" + colorCodeReset);
         console.log(colorCode + meaning + colorCodeReset);
+
+        mainMenu();
     }, 2000)
 }
 
 async function yayOrNay() {
     const question = await askYourQuestion();
     const card = await pickACard();
-    console.clear();
-    console.log(colorCode + "The answer to your question, \"" + question + "\" is...." + colorCodeReset);
+    const theFuture = yesOrNoMessages[card.interpretations.yesOrNo.toLowerCase()].trim();
+    const terminalWidth = process.stdout.columns;
+    let messageToPad = theFuture.split('\n');
+    let pad;
 
-    setTimeout(() => {
-        console.log(colorCode + yesOrNoMessages[card.interpretations.yesOrNo.toLowerCase()] + colorCodeReset);
+    console.log("messagetoPad: " + messageToPad);
+    
+    messageToPad = messageToPad.map((line, index) => {
+        pad = Math.floor((terminalWidth - line.length)/2);
+        return line = ' '.repeat(pad) + line;        
+    });
+
+    messageToPad = messageToPad.join("\n");
+
+    console.clear();
+    console.log(colorCode + "The answer to your question, \"" + colorCode + colorCodeBold + question + colorCodeReset + colorCode + "\" is...." + colorCodeReset);
+
+    let count = 0;
+
+    const pondering = setInterval(() => {
+        readline.cursorTo(process.stdout, 0);
+        readline.clearLine(process.stdout, 0);
+
+        let processingMessage = processingMessages[Math.floor(Math.random() * processingMessages.length)];
+        process.stdout.write(processingMessage);
+    }, 1000)
+
+    setTimeout(async () => {
+        console.clear();
+        console.log(colorCode + messageToPad + colorCodeReset);
         console.log(colorCode + "\n\n(You received " + card.name + ")" + colorCodeReset);
-    }, 2000)
+        clearInterval(pondering);
+        
+        await input ({ message: "Press any key to return to the menu..."});
+        mainMenu();
+    }, 4000)
 }
+
+mainMenu();
---
+++ utilities/processing-messages.js
@@ -0,0 +1,3 @@
+export const processingMessages = [
+    "Scrying the black mirror", "Pondering the orbs", "Gazing into the abyss", "Divining the future", "Reading the tea leaves", "Considering the cards", "Moving the planchette", "Listening to the angels", "Calling to the gods", "Whispering to the daemons", "Visiting the fairies"
+];
